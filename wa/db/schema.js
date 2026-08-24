6b6cd07161c602bdc629745a44be20d37a69bf30
Indra Sah Noeldy <indra87g@proton.me>
2026-08-24 21:33:41 +0700

    cleanup

--- wa/db/schema.js
+++
@@ -1,7 +0,0 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const users = sqliteTable("users", {
-  whatsappId: text("whatsapp_id").primaryKey(),
-  role: text("role").default("user"), // 'user', 'partner'
-  premiumExpiry: integer("premium_expiry").default(0),
-});
